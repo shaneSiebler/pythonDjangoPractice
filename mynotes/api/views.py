a9da3c5e6b9a0d61668a1ac4d5f6ac21c32ad695
Shane <ssiebler21@gmail.com>
2023-11-16 08:13:15 -0700

    updated api routes style with rest framework decorators

--- mynotes/api/views.py
+++ mynotes/api/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+# from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
+@api_view(['GET'])
 def getRoutes(request):
     
      routes = [
@@ -38,4 +41,4 @@ def getRoutes(request):
         },
     ]
      
-     return JsonResponse(routes, safe=False)
+     return Response(routes)
